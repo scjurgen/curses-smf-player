aee7a1865967026300cdf8c18b6c14de7bc2e556
Juergen Schwietering <juergen@orchestraltools.com>
2024-01-08 12:44:53 +0100

    wip

--- curses-key-player.py
+++ curses-key-player.py
@@ -16,30 +16,32 @@ class Settings:
 
     def __init__(self):
         self.home = str(Path.home())
-        self.homedir = f"{self.home}/.curseskeyplay/"
-        if not os.path.isdir(self.homedir):
-            os.mkdir(self.homedir, 0o700)
-        self.settingsFileName = f"{self.homedir}settings.json"
+        self.home_dir = f"{self.home}/.curseskeyplay/"
+        if not os.path.isdir(self.home_dir):
+            os.mkdir(self.home_dir, 0o700)
+        self.settings_file_name = f"{self.home_dir}settings.json"
         try:
-            with open(self.settingsFileName, "r") as f:
+            with open(self.settings_file_name, "r") as f:
                 data = f.read()
-                self.jsonData = json.loads(data)
+                self.json_data = json.loads(data)
         except:
-            self.jsonData = {"home": self.home, "lastworkingdirectory": os.getcwd(), "arpeggiator": False}
-            self.createSettingsFile()
+            self.json_data = {"home": self.home, "lastworkingdirectory": os.getcwd(), "arpeggiator": False}
+            self.create_settings_file()
 
-    def setCurrentWorkingDirectory(self, path:str):
-        self.jsonData["lastworkingdirectory"] = path
-        self.createSettingsFile()
+    def set_current_working_directory(self, path: str):
+        self.json_data["lastworkingdirectory"] = path
+        self.create_settings_file()
 
-    def createSettingsFile(self):
-        with open(self.settingsFileName, "w") as f:
-            s = json.dumps(self.jsonData)
+    def create_settings_file(self):
+        with open(self.settings_file_name, "w") as f:
+            s = json.dumps(self.json_data)
             f.write(s)
 
 
 class App:
     def __init__(self):
+        self.settings = None
+        self.midi_out = None
         self.screen = curses.initscr()
         self.screen.keypad(True)
         curses.noecho()
@@ -51,77 +53,64 @@ class App:
             curses.init_pair(i, i, -1)
         for i in range(8):
             curses.init_pair(i + 8, 0, i)
-        self.infow = 30
-        self.indexfile = 0
-        self.topindex = 0
+        self.info_w = 30
+        self.index_file = 0
+        self.top_index = 0
         self.rows, self.cols = self.screen.getmaxyx()
         self.rows -= 2
-        self.wdir = self.cols - self.infow
+        self.width_directory = self.cols - self.info_w
 
         self.transpose = 0
-        self.baseNote = 48
+        self.baseNote = 12
         self.velocity = 120
-        self.resetScreen()
-        self.activeNotes = {}
-        self.notesOffCounter = 0
-
-
-    def handleNoteOn(self, h: int):
-        if h in self.activeNotes:
-            del self.activeNotes[h]
+        self.reset_screen()
+        self.active_notes = {}
+        self.notes_off_counter = 0
+        self.octave = 3
+        
+    def handle_note_on(self, h: int):
+        h += self.octave * 12
+        if h in self.active_notes:
+            del self.active_notes[h]
             self.midi_out.send_message([0x80, self.baseNote + h, 64])
-            self.screen.addstr(2, h, f'-')
+            self.screen.addstr(2, h, '-')
         else:
-            self.notesOffCounter = 100000
+            self.notes_off_counter = 100000
             self.midi_out.send_message([0x90, self.baseNote + h, self.velocity])
-            self.activeNotes[h] = self.velocity
-            self.screen.addstr(2, h, f'O')
+            self.active_notes[h] = self.velocity
+            self.screen.addstr(2, h, 'O')
 
-    def tickAutoNotesOff(self):
-        if self.notesOffCounter > 0:
-            self.notesOffCounter -= 1
-            if self.notesOffCounter == 0:
-                for key, value in self.activeNotes.items():
+    def tick_auto_notes_off(self):
+        if self.notes_off_counter > 0:
+            self.notes_off_counter -= 1
+            if self.notes_off_counter == 0:
+                for key, value in self.active_notes.items():
                     self.midi_out.send_message([0x80, self.baseNote + key, 64])
 
-    def sendNote(self, k: str):
-        notemap = {
-            'a': 0,
-            'w': 1,
-            's': 2,
-            'e': 3,
-            'd': 4,
-            'f': 5,
-            't': 6,
-            'g': 7,
-            'y': 8,
-            'h': 9,
-            'u': 10,
-            'j': 11,
-            'k': 12,
-            'o': 13,
-            'l': 14,
-            'p': 15,
-            ';': 16,
-            '\'': 17
+    def send_note(self, k: str):
+        key_note_map = {
+            'a': 0, 'w': 1, 's': 2, 'e': 3, 'd': 4, 'f': 5, 't': 6, 'g': 7,
+            'y': 8, 'h': 9, 'u': 10, 'j': 11, 'k': 12, 'o': 13, 'l': 14, 'p': 15,
+            ';': 16, '\'': 17
         }
-        if k in notemap:
-            h = notemap[k]
-            self.handleNoteOn(h)
+        if k in key_note_map:
+            h = key_note_map[k]
+            self.handle_note_on(h)
 
-    def resetScreen(self):
+    def reset_screen(self):
         self.rows, self.cols = self.screen.getmaxyx()
-        self.wdir = self.cols - self.infow
+        self.width_directory = self.cols - self.info_w
         self.rows -= 2
+        self.screen.addstr(2, 1, "Hello")
 
-    def interpretKey(self, key):
-        #self.screen.addstr(self.rows + 1, 0, f'{key}          ')
-        #self.screen.refresh()
+    def interpret_key(self, key):
+        # self.screen.addstr(self.rows + 1, 0, f'{key}          ')
+        # self.screen.refresh()
 
         if 'KEY_UP' == key:
-            pass
+            self.octave += 1
         elif 'KEY_DOWN' == key:
-            pass
+            self.octave -= 1
         elif key in ['+']:
             pass
         elif key in ['-']:
@@ -129,56 +118,57 @@ class App:
         elif key in ['\x1b']:
             return False
         elif key in "asdfghjkl;'wetyuop":
-            self.sendNote(key)
-        self.screen.move(1, self.wdir + self.rows - 2)
-        self.screen.addstr(1, 2, f" {key} {ord(key)}     ")
+            self.send_note(key)
+        self.screen.move(1, self.width_directory + self.rows - 2)
+        # self.screen.addstr(1, 2, f" {key} {ord(key)}     ")
         return True
 
-    def cleanExit(self):
+    def clean_exit(self):
         curses.nocbreak()
         self.screen.keypad(False)
         curses.echo()
         curses.endwin()
 
-    def loadSettings(self):
+    def load_settings(self):
         self.settings = Settings()
         try:
-            os.chdir(self.settings.jsonData["lastworkingdirectory"])
+            os.chdir(self.settings.json_data["lastworkingdirectory"])
         except:
             print("couldn't go to last directory")
-            self.settings.setCurrentWorkingDirectory(os.getcwd())
+            self.settings.set_current_working_directory(os.getcwd())
 
     def run(self) -> bool:
 
         self.midi_out = rtmidi.MidiOut()
         self.midi_out.open_virtual_port("midi-curse")
-        #index = 0
+        # index = 0
         while True:
-            self.tickAutoNotesOff()
-            #index += 1
-            #self.screen.addstr(4, 2, f" {index}     ")
+            self.tick_auto_notes_off()
+            # index += 1
+            # self.screen.addstr(4, 2, f" {index}     ")
             try:
                 time.sleep(0.001)
                 key = self.screen.getkey()
                 if key != -1:
                     if key == 'KEY_RESIZE':
-                        self.cleanExit()
+                        self.clean_exit()
                         return True
-                    elif not self.interpretKey(key):
-                        self.cleanExit()
-                        print("bBck to shell...")
+                    elif not self.interpret_key(key):
+                        self.clean_exit()
+                        print("back to shell...")
                         return False
 
             except Exception as e:
-                #print("Exception raised:", e)
+                # print("Exception raised:", e)
                 time.sleep(0.001)
 
-def main(cursesWindow):
+
+def main(curses_window):
     app = App()
-    while(app.run()):
-        time.sleep(0.05)
+    while (app.run()):
+        time.sleep(0.01)
         app = App()
+
 
 if __name__ == '__main__':
     wrapper(main)
-
